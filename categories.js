93c833e050c7766de865ae59dae92e36e92a4abb
devSudhir <sudhir.samantaray@yahoo.com>
2021-06-17 04:14:31 +0530

    categories.js modified

--- categories.js
+++ categories.js
@@ -15,15 +15,46 @@ function showChildList(index) {
     childDiv.innerHTML = null;
     let ul = document.createElement("ul");
     for (let i = 0; i < childArray[index].length; i += 2){
-        console.log(childArray[index][i + 1])
         let li = document.createElement("li");
-        li.innerHTML = `<a onmouseover="showGrandChildList(${index})" href=${childArray[index][i]}>${childArray[index][i + 1]}</a>`;
+        li.innerHTML = `<a  onmouseover="showGrandChildList(${index},${i / 2})" href=${childArray[index][i]}>${childArray[index][i + 1]}</a>`;
         ul.appendChild(li);
     } 
     childDiv.appendChild(ul);
 }
 
-function showGrandChildList(index) {
+let grandChildArray = [[["#", "JavaScript", "#", "React", "#", "css",
+    "#", "Angular","#", "Django",
+    "#", "PHP","#", "Node.js","#","Wordpress"],["#","Python","#","Machine learning","#","Deep learning","#","Data Analysis","#","Artificial Intelligence","#","R(programming language)","#","TensorFlow","#","Statistics"]]];
+function showGrandChildList(childIndex, grandChildIndex) {
+    // console.log(grandChildArray[childIndex][grandChildIndex], childIndex, grandChildIndex);
     let grandChildDiv = document.getElementById("grandChild_div");
-    grandChildDiv.style.display = "block"
+    grandChildDiv.style.display = "block";
+    grandChildDiv.innerHTML = null;
+    let ul = document.createElement("ul");
+    for (let i = 0; i < grandChildArray[childIndex][grandChildIndex].length; i += 2){
+        // console.log(grandChildArray[childIndex][grandChildIndex][i + 1], i);
+        
+        let li = document.createElement("li");
+        li.innerHTML = `<a href=${grandChildArray[childIndex][grandChildIndex][i]}>${grandChildArray[childIndex][grandChildIndex][i + 1]}</a>`;
+        ul.appendChild(li);
+    }
+    let h3 = document.createElement("h3");
+    h3.textContent = "Popular topics";
+
+    grandChildDiv.appendChild(h3);
+    grandChildDiv.appendChild(ul);
 }
+/* function disableDisplayParent() {
+    let parentDiv = document.getElementById("parent_div");
+    parentDiv.style.display = "none";
+} */
+
+/* function disableDisplayChild() {
+    let childDiv = document.getElementById("child_div");
+    childDiv.style.display = "none";
+}
+
+function disableDisplayGrandChild() {
+    let grandChildDiv = document.getElementById("grandChild_div");
+    grandChildDiv.style.display = "none";
+} */
